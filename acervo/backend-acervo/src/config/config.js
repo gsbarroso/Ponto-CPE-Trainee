9cc4fa9f298f068c23c7cc4d237844b17c593809
Gustavo Silvestre Barroso <gustavo2804sb@gmail.com>
2025-06-05 15:25:07 -0300

    Update config.js

--- acervo/backend-acervo/src/config/config.js
+++ acervo/backend-acervo/src/config/config.js
@@ -3,4 +3,4 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const JWT_SECRET = process.env.JWT_SECRET || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9';
-export const MONGO_URL = process.env.MONGO_URL || 'mongodb+srv://gustavo05sb:28042005Gu$B@sistemaponto.r7cxsbm.mongodb.net/sistemaPonto?retryWrites=true&w=majority';
+export const MONGO_URL = process.env.MONGO_URL 
